249e4e56bf9f4f99698eb9b3efa893952ad67120
Dmytro Kovbasiuk <k0vbasyuk.dim0n@gmail.com>
2024-10-04 08:56:10 +0300

    1

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ import CssBaseline from "@mui/material/CssBaseline";
 import Box from "@mui/material/Box";
 import MenuItem from "@mui/material/MenuItem";
 import Select from "@mui/material/Select";
-import Typography from "@mui/material/Typography";
 
 function MyApp({ mode, setMode }) {
   return (
